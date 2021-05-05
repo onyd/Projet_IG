4d35abdd68e4f1b6e10ba704783b85436a7ec75c
Anthony Dard <anthony.dard@grenoble-inp.org>
2021-05-05 18:58:19 +0200

    ei_fill + map_rgba and draw_polyline corrected

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -1,26 +1,20 @@
 #include "ei_draw.h"
+#include "stdio.h"
 
 uint32_t ei_map_rgba(ei_surface_t surface, ei_color_t color) {
-    int *ir = NULL;
-    int *ig = NULL;
-    int *ib = NULL;
-    int *ia = NULL;
-    hw_surface_get_channel_indices(surface, ir, ig, ib, ia);
+    int ir, ig, ib, ia;
+
+    hw_surface_get_channel_indices(surface, &ir, &ig, &ib, &ia);
     uint32_t r, g, b, a = 0;
-    if (*ir != -1) {
-        r = color.red << (8 * (*ir));
+
+    r = (uint32_t)color.red << (8 * ir);
+    g = (uint32_t)color.green << (8 * ig);
+    b = (uint32_t)color.blue << (8 * ib);
+    if (ia != -1) {
+        return r | g | b | a;
+    } else {
+        return r | g | b;
     }
-    if (*ig != -1) {
-        g = color.green << (8 * (*ig));
-    }
-    if (*ib != -1) {
-        b = color.blue << (8 * (*ib));
-    }
-    if (*ia != -1) {
-        a = color.alpha << (8 * (*ia));
-    }
-    uint32_t c = r || g || b || a;
-    return c;
 }
 
 void ei_draw_polyline(ei_surface_t surface,
@@ -30,8 +24,11 @@ void ei_draw_polyline(ei_surface_t surface,
     const ei_linked_point_t *prev = first_point;
     const ei_linked_point_t *current = first_point->next;
 
-    uint32_t *s = (uint32_t *) hw_surface_get_buffer(surface);
+    hw_surface_lock(surface);
+    uint32_t *pixels = (uint32_t *) hw_surface_get_buffer(surface);
     ei_size_t size = hw_surface_get_size(surface);
+
+    // Draw all lines between points
     while (current != NULL) {
         /* Bresenham */
         int dx = current->point.x - prev->point.x;
@@ -43,10 +40,10 @@ void ei_draw_polyline(ei_surface_t surface,
         while (x != current->point.x && y != current->point.y) {
             if (dx > 0) {
                 x++;
-                E += dx;
+                E += dy;
             } else {
                 x--;
-                E -= dx;
+                E -= dy;
             }
             if (2 * E > dx) {
                 if (dy > 0) {
@@ -57,12 +54,15 @@ void ei_draw_polyline(ei_surface_t surface,
                     E += dx;
                 }
             }
+            // Draw pixel in the buffer
             uint32_t c = ei_map_rgba(surface, color);
-            s[x + size.width * y] = c;
+            pixels[x + size.width * y] = c;
         }
         prev = current;
-        current = first_point->next;
+        current = current->next;
+
     }
+    hw_surface_unlock(surface);
 }
 
 
@@ -85,7 +85,13 @@ void ei_draw_text(ei_surface_t surface,
 void ei_fill(ei_surface_t surface,
              const ei_color_t *color,
              const ei_rect_t *clipper) {
-
+    hw_surface_lock(surface);
+    uint32_t *pixels = (uint32_t *) hw_surface_get_buffer(surface);
+    ei_size_t size = hw_surface_get_size(surface);
+    for (uint32_t i = 0; i< size.width*size.height;i++){
+        pixels[i] = "0xFFFFFFFF";
+    }
+    hw_surface_unlock(surface);
 }
 
 int ei_copy_surface(ei_surface_t destination,
