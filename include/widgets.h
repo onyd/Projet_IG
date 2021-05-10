494e6843ae72469f3cd15b83777055d5ab7ad58c
Martin Gangand <martin.gangand@grenoble-inp.org>
2021-05-10 14:23:22 +0200

    ei_app_create

---
+++ include/widgets.h
@@ -0,0 +1,15 @@
+#ifndef PROJETC_IG_WIDGETS_H
+#define PROJETC_IG_WIDGETS_H
+#include "widgets.h"
+#include "ei_widgetclass.h"
+#include "ei_widget.h"
+
+
+typedef struct ei_button_t {
+    struct ei_widget_t widget;
+    // Paramètres
+}ei_button_t;
+
+ei_widgetclass_t* button_class;
+ei_widgetclass_t* widget_class;
+#endif //PROJETC_IG_WIDGETS_H
---
+++ src/ei_application.c
@@ -0,0 +1,35 @@
+#include "hw_interface.h"
+#include "widgets.h"
+#include "ei_widgetclass.h"
+#include "stdbool.h"
+#include "string.h"
+#include "ei_application.h"
+
+void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen){
+    hw_init();
+    strcpy(button_class -> name, "button");
+    strcpy(widget_class -> name, "widget");
+    ei_widgetclass_register(&button_class);
+
+    if (fullscreen == false){
+        hw_create_window(main_window_size, EI_FALSE);
+    }
+    else{
+        hw_create_window(hw_surface_get_size(ei_app_root_surface()), EI_TRUE);
+    }
+}
+
+void ei_app_free(void);
+
+void ei_app_run(void);
+
+void ei_app_invalidate_rect(ei_rect_t* rect);
+
+void ei_app_quit_request(void);
+
+ei_widget_t* ei_app_root_widget(void);
+
+
+ei_surface_t ei_app_root_surface(void);
+
+
