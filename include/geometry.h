698fc468ceb3aa31a2464f4617307238fd696e7d
Valentin Laclautre <valentin.laclautre@grenoble-inp.org>
2021-05-10 17:21:24 +0200

    correction lines

--- include/geometry.h
+++ include/geometry.h
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include "stdio.h"
 #include "ei_draw.h"
+#include "ei_widget.h"
 
 static const float pi = 3.1415927;
 
@@ -16,6 +17,8 @@ ei_linked_point_t *arc(const ei_point_t *c, uint32_t r, float start_angle, float
 ei_linked_point_t *rounded_frame(ei_rect_t button_rect, uint32_t rayon, uint32_t N, int param);
 
 void draw_button(ei_surface_t surface, ei_rect_t *clipper, ei_rect_t button_rect, ei_color_t color, int radius, bool etat);
+
+void free_rounded_frame(ei_linked_point_t *points);
 
 ei_bool_t intersection(const ei_rect_t *r1, const ei_rect_t *r2, ei_rect_t *result);
 
