71be4e7d6d230bbae8668b51559973be17b43bbe
Valentin Laclautre <valentin.laclautre@grenoble-inp.org>
2021-05-12 13:57:36 +0200

    ei_place done

--- src/ei_placer.c
+++ src/ei_placer.c
@@ -53,31 +53,6 @@ void		ei_place	(struct ei_widget_t*	widget,
     params->rh_data = (rel_height != NULL) ? *rel_height : params->rh_data;
     ei_placer_run(widget);
 
-    ei_point_t anchor_point;
-    switch (*anchor) {
-        case ei_anc_none:
-            break;
-        case ei_anc_center:
-            anchor_point = absolute_coords(ei_rect(ei_point(*x, *y), used_size), 0.5, 0.5);
-            break;
-        case ei_anc_north:
-            break;
-        case ei_anc_northeast:
-            break;
-        case ei_anc_east:
-            break;
-        case ei_anc_southeast:
-            break;
-        case ei_anc_south:
-            break;
-        case ei_anc_southwest:
-            break;
-        case ei_anc_west:
-            break;
-        case ei_anc_northwest:
-            break;
-
-    }
 }
 
 void ei_placer_run(struct ei_widget_t *widget) {
