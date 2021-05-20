7937d1c7984a7ca04065059e7ae0cfe21d57ea67
Valentin Laclautre <valentin.laclautre@grenoble-inp.org>
2021-05-20 16:43:48 +0200

    adding toplevel.c and frame.c

--- tests/frame.c
+++ tests/frame.c
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 
 	ei_widget_t*	frame;
 	ei_size_t	frame_size		= {300,200};
-	int		frame_x			= 200;
+	int		frame_x			= 150;
 	int		frame_y			= 200;
 	ei_color_t	frame_color		= {0x88, 0x88, 0x88, 0xff};
 	ei_relief_t	frame_relief		= ei_relief_raised;
@@ -29,36 +29,12 @@ int main(int argc, char** argv)
 	ei_app_create(screen_size, EI_FALSE);
 	ei_frame_configure(ei_app_root_widget(), NULL, &root_bgcol, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-	/*create a toplevel */
-    int		toplevel_x			= 100;
-    int		toplevel_y			= 150;
-	ei_widget_t *toplevel = ei_widget_create("toplevel", ei_app_root_widget(), NULL, NULL);
-    ei_toplevel_configure(toplevel, &frame_size, NULL,
-                           NULL, NULL, NULL, NULL, NULL);
-    ei_place(toplevel, NULL, &toplevel_x, &toplevel_y, NULL, NULL, NULL, NULL, NULL, NULL );
-    int width = 300;
-    int height = 200;
-    ei_place(toplevel, NULL, NULL, NULL, &width, &height, NULL, NULL, NULL, NULL );
 	/* Create, configure and place the frame on screen. */
-    // text on the frame
-	char *text2 = "Toto";
-    ei_anchor_t text_anchor2 = ei_anc_center;
-	frame = ei_widget_create("frame", toplevel, NULL, NULL);
+	frame = ei_widget_create("frame", ei_app_root_widget(), NULL, NULL);
 	ei_frame_configure	(frame, &frame_size, &frame_color,
-				 &frame_border_width, &frame_relief, &text2, NULL, NULL, &text_anchor2,
+				 &frame_border_width, &frame_relief, NULL, NULL, NULL, NULL,
 				 NULL, NULL, NULL);
-	float relative = 0;
-	float rel_size = 1;
-	ei_anchor_t anchor = ei_anc_northwest;
-	ei_place(frame, NULL, NULL, NULL, NULL, NULL, NULL, NULL, &rel_size, &rel_size);
-
-	/*second toplevel */
-    int		toplevel_x_2			= 200;
-    int		toplevel_y_2			= 300;
-    ei_widget_t *toplevel_2 = ei_widget_create("toplevel", ei_app_root_widget(), NULL, NULL);
-    ei_toplevel_configure(toplevel_2, &frame_size, NULL,
-                          NULL, NULL, NULL, NULL, NULL);
-    ei_place(toplevel_2, NULL, &toplevel_x_2, &toplevel_y_2, NULL, NULL, NULL, NULL, NULL, NULL );
+	ei_place(frame, NULL, &frame_x, &frame_y, NULL, NULL, NULL, NULL, NULL, NULL );
 
 	/* Run the application's main loop. */
 	ei_app_run();
